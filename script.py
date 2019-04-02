6d0c91ff91a98f4bb7ddebb3595969480d671144
cesartalves <cesartorralvoa@hotmail.com>
2019-04-02 15:43:11 -0300

    add decorator to only build certain targets move some variables to environment ones update json schema - event["body-json"]

--- script.py
+++ script.py
@@ -5,15 +5,30 @@ import zipfile
 import StringIO
 import os
 
+def conditionalBuildTarget(targetName):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            if args[0]["body-json"]["buildTargetName"] != targetName:
+        	    return "Skipping deploy"
+            else:
+                return function(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@conditionalBuildTarget(os.environ.get('BUILD_TARGET_TO_DEPLOY', 'HandsOnMedical'))
 def lambda_handler(event, context):
-	conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY)
-	tokenId = "Basic " + UNITY_API_KEY
-	s3bucket = S3_BUCKET_NAME
+	conn = tinys3.Connection(
+	    os.environ['S3_ACCESS_KEY'], os.environ['S3_SECRET_KEY'])
+	tokenId = "Basic " + os.environ['UNITY_API_KEY']
+	s3bucket = os.environ['S3_BUCKET_NAME']
 	print event
-	buildLink = event["links"]["api_self"]["href"]
+
+	buildLink = event["body-json"]["links"]["api_self"]["href"]
 
 	authPayload = {"Authorization": tokenId}
-	buildData = requests.get("https://build-api.cloud.unity3d.com" + buildLink, headers=authPayload)
+	buildData = requests.get(
+	    "https://build-api.cloud.unity3d.com" + buildLink, headers=authPayload)
 	primaryLink = json.loads(buildData.text)["links"]["download_primary"]["href"]
 	print primaryLink
 	results = requests.get(primaryLink)
@@ -24,16 +39,16 @@ def lambda_handler(event, context):
 	print dirName
 	baseDir = "/tmp/" + dirName
 
-	f = open(baseDir + "/index.html",'rb') 
-	conn.upload('index.html',f,s3bucket)
+	f = open(baseDir + "/index.html", 'rb')
+	conn.upload('index.html', f, s3bucket)
 
-	files = os.listdir(baseDir + "/Build") 
+	files = os.listdir(baseDir + "/Build")
 	for filename in files:
-		f = open(baseDir + "/Build/" + filename,'rb') 
-		conn.upload("Build/" + filename,f,s3bucket) 
-	files = os.listdir(baseDir + "/TemplateData") 
+		f = open(baseDir + "/Build/" + filename, 'rb')
+		conn.upload("Build/" + filename, f, s3bucket)
+	files = os.listdir(baseDir + "/TemplateData")
 	for filename in files:
-		f = open(baseDir + "/TemplateData/" + filename,'rb') 
-		conn.upload("TemplateData/" + filename,f,s3bucket)
+		f = open(baseDir + "/TemplateData/" + filename, 'rb')
+		conn.upload("TemplateData/" + filename, f, s3bucket)
 
-	return "Done"
+	return event["body-json"]["buildTargetName"]
